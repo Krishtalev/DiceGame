9dcd0e2589341b810ad04f5ff02ea2ce3cf0b4d7
Krishtalev <47716836+Krishtalev@users.noreply.github.com>
2021-05-18 10:53:03 +0300

    Add files via upload

---
+++ lab10/lab10/Form1.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace lab10
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        public bool isWin(int[] you, int[] opponent)
+        {
+            if (you[0] > opponent[0]) return true;
+            if (you[0] == opponent[0] && you[1] >= opponent[1]) return true;
+            return false;
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Player opponent = new Player();
+            int[] opponent_dices = opponent.cheat_roll();
+            label1.Text = Convert.ToString(opponent_dices[0]);
+            label2.Text = Convert.ToString(opponent_dices[1]);
+            label3.Text = Convert.ToString(opponent_dices[2]);
+            label4.Text = Convert.ToString(opponent_dices[3]);
+            label5.Text = Convert.ToString(opponent_dices[4]);
+
+            Player you = new Player();
+            int[] your_dices = you.common_roll();
+            label6.Text = Convert.ToString(your_dices[0]);
+            label7.Text = Convert.ToString(your_dices[1]);
+            label8.Text = Convert.ToString(your_dices[2]);
+            label9.Text = Convert.ToString(your_dices[3]);
+            label10.Text = Convert.ToString(your_dices[4]);
+
+            bool win = isWin(you.check_combination(), opponent.check_combination());
+            if (win) label11.Text = "You won";
+            if (!win) label11.Text = "You lost";
+        }
+    }
+}
---
+++ lab10/lab10/Player.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lab10
+{
+    class Player
+    {
+        public double[] probs;
+        public int[] dices;
+
+        public Player()
+        {
+            dices = new int[5];
+            probs = new double[6];
+            for (int i = 0; i < 6; i++)
+            {
+                probs[i] = 1.0 / 6;
+            }
+        }
+        public int roll()
+        {
+            Random rnd = new Random();
+            double A = rnd.NextDouble();
+            int event_id = -1;
+            do
+            {
+               event_id++;
+               A -= probs[event_id];
+            } while (A > 0);
+            return event_id+1;
+        }
+
+        public int[] cheat_roll()
+        {
+            int win_number = roll();
+            dices[0] = win_number;
+            int win_id = win_number-1;
+            for (int i = 0; i < 6; i++)
+            {
+                probs[i] = 1.0 / 8;
+            }
+            probs[win_id] = 3.0 / 8;
+            for (int i = 1; i < 5; i++)
+            {
+                dices[i] = roll();
+            }
+            for (int i = 0; i < 6; i++)
+            {
+                probs[i] = 1.0 / 6;
+            }
+            return dices;
+        }
+
+        public int[] common_roll()
+        {
+            for (int i = 0; i < 5; i++)
+            {
+                dices[i] = roll();
+            }
+            return dices;
+        }
+
+        public int[] check_combination()
+        {
+            var max_count = 0;
+            var elder = 0;
+            int[] comb = new int[6];
+            foreach (var dice in dices)
+            {
+                comb[dice-1]++;
+            }
+            for (int i = 0; i < 6; i++)
+            {
+                if (comb[i]>=max_count)
+                {
+                    max_count = comb[i];
+                    elder = i;
+                }
+            }
+            int[] ans = { max_count, elder };
+            return (ans);
+        }
+
+    }
+}
